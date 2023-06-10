781f7a28fcad189f5eb4ddc32df696e4069b8a79
BennyG117 <bengomezp@gmail.com>
2023-06-09 18:58:15 -0700

    Update - Comments & Css

--- server.py
+++ server.py
@@ -7,14 +7,6 @@ app.secret_key = "quirkless"
 def main_page():
     return render_template("index.html")
 
-# # ('/process)route function that submits data that use inputs when button is pressed 
-# @app.route('/process', methods = ['POST'])
-# def submit_data():
-#     session['user'] = request.form['user']
-#     return redirect('/index.html')
-
-#!or?
-
 @app.route('/process', methods = ['POST'])
 def submit_data():
     session['name'] = request.form['name']
@@ -22,21 +14,13 @@ def submit_data():
     session['language'] = request.form['language']
     session['comment'] = request.form['comment']
 
-
     print(request.form)
     return redirect('/result')
-
-
-
-# save the form data into session ???
-
-
 
 #localhost:5001/result ('/result') - app route that redirects to "indexR.html" (results page) that shows user submitted info by post
 @app.route('/result')
 def show_results():
     return render_template('indexR.html')
-
 
 if __name__=="__main__":
     app.run(debug=True, port=5001)
